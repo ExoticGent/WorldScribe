12d441eec8a6499fd501a8b362356f7f6e9b4d92
ExoticGent <Eammon567@gmail.com>
2026-04-24 01:47:28 +0100

    Add Firebase web app config

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -65,4 +62,15 @@ class DefaultFirebaseOptions {
     storageBucket: 'worldscribe-9c753.firebasestorage.app',
     iosBundleId: 'com.worldscribe.worldscribe',
   );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyBxWBheoskutu_wJ9sJyHvE9mcX-MqddWM',
+    appId: '1:625579797661:web:8a0b0a54084eb5f4a07874',
+    messagingSenderId: '625579797661',
+    projectId: 'worldscribe-9c753',
+    authDomain: 'worldscribe-9c753.firebaseapp.com',
+    storageBucket: 'worldscribe-9c753.firebasestorage.app',
+    measurementId: 'G-LTSMLWBEP8',
+  );
+
 }
